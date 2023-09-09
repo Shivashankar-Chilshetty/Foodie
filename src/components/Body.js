c98b74264fe08bb38e91a8fffc090c0f0c2e1cd0
Shivashankar-Chilshetty <chilshetty77@gmail.com>
2023-09-09 12:31:15 +0530

    making an api call to fetch data from swiggy api using useEffect hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import RestaurantCard from './RestaurantCard';
 import { resList } from '../utils/mockData';
 
 const Body = () => {
     const [listOfRestaurants, setListOfRestraunt] = useState(resList);
+    useEffect(() => {
+        fetchData()
+    }, []);
+    const fetchData = async() => {
+        let data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8463544&lng=77.6514648&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+        let jsonData = await data.json();
+        console.log('json data', jsonData)
+    }
     return (
         <div className='body'>
             <div className = 'filter'>
