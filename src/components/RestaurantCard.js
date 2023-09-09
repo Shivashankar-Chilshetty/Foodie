2a89a68831eb2e43f8c96a0ac49e36969342394c
Shivashankar-Chilshetty <chilshetty77@gmail.com>
2023-09-09 11:00:11 +0530

    split the code into multiple files

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,14 @@
+import {IMG_CDN_URL} from '../utils/constants'
+const RestaurantCard = ({ name, locality, cuisines, avgRatingString, cloudinaryImageId }) => {
+    console.log(cuisines)
+    return (
+        <div className="card">
+            <img src={IMG_CDN_URL + cloudinaryImageId} />
+            <h3>{name}</h3>
+            <h3>{locality}</h3>
+            <h3>{cuisines.join(', ')}</h3>
+            <h3>{avgRatingString}</h3>
+        </div>
+    )
+}
+export default RestaurantCard;
