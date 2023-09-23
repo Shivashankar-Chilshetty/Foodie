711843ce604d3d7e8b9c692ef5d33e2d526e829a
Shivashankar-Chilshetty <chilshetty77@gmail.com>
2023-09-23 16:45:17 +0530

    implemented accordian to display menu details & butify the restaurant menu using tailwind classes

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,7 +1,7 @@
 import Shimmer from "./Shimmer";
 import { useParams } from 'react-router-dom';
 import useRestaurantMenu from "../utils/useRestaurantMenu";
-
+import RestaurantCategory from './RestaurantCategory';
 
 const RestaurantMenu = () => {
     const { resId } = useParams();
@@ -9,21 +9,18 @@ const RestaurantMenu = () => {
     if (resInfo == null) return <Shimmer />
     const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info;
     const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-    console.log('itemscard--', itemCards)
+    const categories =
+        resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+            (c) =>
+                c.card?.card?.['@type'] == 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory'
+        )
     return (
-        <div>
-            <h1>{name}</h1>
-            <p>{cuisines.join(',')}</p>
-            <p>{costForTwoMessage}</p>
-            <h2>Menu:</h2>
-            <ul>
-                {itemCards.map((item) => (
-                    <li key={item.card.info.id}>
-                        {item.card.info.name} - {' Rs:'}
-                        {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
-                    </li>
-                ))}
-            </ul>
+        <div className='text-center'>
+            <h1 className='font-bold my-6 text-2xl'>{name}</h1>
+            <p className='font-bold text-lg'>{cuisines.join(', ')} - {costForTwoMessage}</p>
+            {categories.map((category) => (
+                <RestaurantCategory key={category?.card?.card.title} data={category?.card?.card} />
+            ))}
         </div>
     )
 }
